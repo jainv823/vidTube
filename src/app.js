19631131c6be145b019b7759f73359c267a19567
Vinay jain <jainv823@gmail.com>
2025-05-06 17:36:03 +0530

    connect to mongodb using mongoose

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
 import express from "express";
-import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const morganFormat = ":method :url :status :response-time ms";
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(process.env.PORT);
-  
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// common middlewares
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.get("/test", (req, res) => {
+  logger.info("A test get request was sent");
+  res.status(200).send("THIS IS A TEST ROUTE SERVER WORKING NORMALLY");
 });
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
-import express from "express";
 import { app } from "./app.js";
-import logger from "./logger.js";
-import morgan from "morgan";
+import "dotenv/config";
+import connectDB from "./db/index.js";
 
-const morganFormat = ":method :url :status :response-time ms";
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
-  })
-);
-
-app.get("/test", (req, res) => {
-  logger.info("A test get request was sent");
-  res.status(200).send("THIS IS A TEST ROUTE SERVER WORKING NORMALLY");
-});
+connectDB()
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log(`MongoDB connection error: ${err}`);
+  });
